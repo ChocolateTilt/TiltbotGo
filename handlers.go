e9e6c6d74c07ddee166b3c52ce94e9f8595e29cc
ChocolateTilt <dmhansen65@gmail.com>
2023-12-22 01:39:59 -0500

    Complete refactor to make more readable

--- handlers.go
+++ handlers.go
@@ -7,148 +7,91 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+var searchType QuoteType
+
+var quoteHandler = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate, options []*discordgo.ApplicationCommandInteractionDataOption){
+	"count": func(s *discordgo.Session, i *discordgo.InteractionCreate, options []*discordgo.ApplicationCommandInteractionDataOption) {
+		var countType QuoteType = "full"
+		count, err := countType.quoteCount("")
+		if err != nil {
+			sendErr(s, i, err)
+			return
+		}
+		sendEmbed(s, i, "Quote Count", []*discordgo.MessageEmbedField{
+			{Name: "Total Quotes", Value: fmt.Sprintf("%d", count)},
+		})
+	},
+	"add": func(s *discordgo.Session, i *discordgo.InteractionCreate, options []*discordgo.ApplicationCommandInteractionDataOption) {
+		quote := options[0].Options[0].StringValue()
+		quotee := options[0].Options[1].UserValue(s)
+		time, err := discordgo.SnowflakeTimestamp(i.ID)
+		if err != nil {
+			sendErr(s, i, err)
+			return
+		}
+		quoteSave := Quote{
+			ID:        primitive.NewObjectID(),
+			Quote:     quote,
+			Quotee:    fmt.Sprintf("<@%v>", quotee.ID),
+			Quoter:    fmt.Sprintf("<@%v>", i.Member.User.ID),
+			CreatedAt: time,
+		}
+		err = createQuote(quoteSave)
+		if err != nil {
+			sendErr(s, i, err)
+			return
+		}
+	},
+	"leaderboard": func(s *discordgo.Session, i *discordgo.InteractionCreate, options []*discordgo.ApplicationCommandInteractionDataOption) {
+		leaderboard, err := getLeaderboard()
+		if err != nil {
+			sendErr(s, i, err)
+		}
+		sendEmbed(s, i, "Quote Leaderboard", []*discordgo.MessageEmbedField{
+			{Name: "All-time", Value: leaderboard},
+		})
+	},
+	"user": func(s *discordgo.Session, i *discordgo.InteractionCreate, options []*discordgo.ApplicationCommandInteractionDataOption) {
+		searchType = "user"
+		quotee := options[0].Options[0].UserValue(s)
+		userID := fmt.Sprintf("<@%v>", quotee.ID)
+		quote, err := searchType.getQuote(userID)
+		if err != nil {
+			sendErr(s, i, err)
+		}
+		sendEmbed(s, i, "Random Quote", quoteFields(quote))
+	},
+	"latest": func(s *discordgo.Session, i *discordgo.InteractionCreate, options []*discordgo.ApplicationCommandInteractionDataOption) {
+		searchType = "latest"
+		quote, err := searchType.getQuote("")
+		if err != nil {
+			sendErr(s, i, err)
+		}
+		sendEmbed(s, i, "Latest Quote", quoteFields(quote))
+	},
+	"random": func(s *discordgo.Session, i *discordgo.InteractionCreate, options []*discordgo.ApplicationCommandInteractionDataOption) {
+		searchType = "rand"
+		quote, err := searchType.getQuote("")
+		if err != nil {
+			sendErr(s, i, err)
+		}
+		sendEmbed(s, i, "Random Quote", quoteFields(quote))
+	},
+}
+
 // commandHandlers is a map of all available Discord slash command handlers
 var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+
 	"quote": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		options := i.ApplicationCommandData().Options
 		subCommand := options[0].Name
-		var searchType QuoteType
 
-		switch subCommand {
-		case "count":
-			var countType QuoteType = "full"
-			count, err := countType.quoteCount("")
-			if err != nil {
-				sendErrToDiscord(s, i, err)
-				return
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{
-						{
-							Title: "Quote Count",
-							Color: 3093151, // dark blue
-							Fields: []*discordgo.MessageEmbedField{
-								{Name: "Total Quotes", Value: fmt.Sprintf("%d", count)},
-							},
-						},
-					},
-				},
-			})
-		case "add":
-			quote := options[0].Options[0].StringValue()
-			quotee := options[0].Options[1].UserValue(s)
-			time, err := discordgo.SnowflakeTimestamp(i.ID)
-			if err != nil {
-				sendErrToDiscord(s, i, err)
-				return
-			}
-			quoteSave := Quote{
-				ID:        primitive.NewObjectID(),
-				Quote:     quote,
-				Quotee:    fmt.Sprintf("<@%v>", quotee.ID),
-				Quoter:    fmt.Sprintf("<@%v>", i.Member.User.ID),
-				CreatedAt: time,
-			}
-			err = createQuote(quoteSave)
-			if err != nil {
-				sendErrToDiscord(s, i, err)
-				return
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{
-						{
-							Title:  "New Quote",
-							Color:  3093151, // dark blue
-							Fields: quoteFields(quoteSave),
-						},
-					},
-				},
-			})
-		case "random":
-			searchType = "rand"
-			quote, err := searchType.getQuote("")
-			if err != nil {
-				sendErrToDiscord(s, i, err)
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{
-						{
-							Title:  "Random Quote",
-							Color:  3093151, // dark blue
-							Fields: quoteFields(quote),
-						},
-					},
-				},
-			})
-		case "latest":
-			searchType = "latest"
-			quote, err := searchType.getQuote("")
-			if err != nil {
-				sendErrToDiscord(s, i, err)
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{
-						{
-							Title:  "Latest Quote",
-							Color:  3093151, // dark blue
-							Fields: quoteFields(quote),
-						},
-					},
-				},
-			})
-		case "user":
-			searchType = "user"
-			quotee := options[0].Options[0].UserValue(s)
-			userID := fmt.Sprintf("<@%v>", quotee.ID)
-			quote, err := searchType.getQuote(userID)
-			if err != nil {
-				sendErrToDiscord(s, i, err)
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{
-						{
-							Title:  "Random Quote",
-							Color:  3093151, // dark blue
-							Fields: quoteFields(quote),
-						},
-					},
-				},
-			})
-		case "leaderboard":
-			leaderboard, err := getLeaderboard()
-			if err != nil {
-				sendErrToDiscord(s, i, err)
-			}
-
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{
-						{
-							Title: "Quote Leaderboard",
-							Color: 3093151, // dark blue
-							Fields: []*discordgo.MessageEmbedField{
-								{Name: "All-time", Value: leaderboard},
-							},
-						},
-					},
-				},
-			})
+		handler, ok := quoteHandler[subCommand]
+		if !ok {
+			sendErr(s, i, fmt.Errorf("unknown sub-command: %s", subCommand))
+			return
 		}
+
+		handler(s, i, options)
 	},
 }
